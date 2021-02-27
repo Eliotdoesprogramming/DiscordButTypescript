1f01d2455e50ec3568bf2ba8b70fe8a899ad2512
eliotdoesprogramming <zubdoob@gmail.com>
2021-02-27 15:13:43 -0600

    test

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export let token : string = "Nzk5ODI1MTMyNDgyNTkyNzkw.YAJNMw.SeZGaLwyIjtxZMHuAJpoJB7AwCA";
+export let token : string = "Nzk5ODI1MTMyNDgyNTkyNzkw.YAJNMw.0-JXoHoUF0drxaCDct2ELKdt92s";
 export let prefix : string = "!";
 export let owners: string|string[]|undefined = [];
